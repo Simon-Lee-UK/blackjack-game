7312d636f38313f94eb54406360c6ff762c6778e
[Simon Lee] <[simonalee96@gmail.com]>
2019-10-12 10:09:27 +0100

    Added input arguments and print method to Card class

--- card.py
+++ card.py
@@ -7,5 +7,16 @@ import numpy as np
 class Card:
     """ A class defining the properties of a card object.
     """
-    def __init__(self):
-        self._suit_array = []
+    def __init__(self, input_suit, input_rank, input_value, input_deck_num):
+        self._suit = input_suit
+        self._rank = input_rank
+        self._value = input_value
+        self._deck_num = input_deck_num + 1
+
+    def print_all_details(self):
+        if self._rank == 'Ace':
+            print('{} of {} (Value = {} or {}, Deck# = {})'
+                  .format(self._rank, self._suit, str(self._value[0]), str(self._value[1]), str(self._deck_num)))
+        else:
+            print('{} of {} (Value = {}, Deck# = {})'
+                  .format(self._rank, self._suit, str(self._value), str(self._deck_num)))
